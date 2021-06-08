33940731260cbf39c9103aa6fbdf6d68fcf7e996
David Wong <davidwong.crypto@gmail.com>
2021-06-07 20:02:42 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# Fromager
+
+Fromager helps you format your codebase.
+
+## Installation & Usage
+
+If you have [Opam](https://opam.ocaml.org/), the package manger of OCaml, simply do:
+
+```console
+$ opam install fromager
+$ fromager
+```
+
+It's that simple!
+
+## Configuring Fromager
+
+By default, fromager ignore any directory starting with an underscore (`_opam`, `_build`, `_coverage`, etc.).
+
+You can optionally´ create a `fromage.toml` file at the root of your project:
+
+```toml
+[config]
+ignored_files = []
+ignored_dirs = ["some", "ignored", "directories]
+```
+
+## Inside a Github action
+
+```yml
+name: Run Fromager
+
+on:
+  [pull_request]
+
+jobs:
+  run_fromager:
+    strategy:
+      fail-fast: false
+      matrix:
+        os:
+          - macos-latest
+          - ubuntu-latest
+        ocaml-version:
+          - 4.07.1
+
+    runs-on: ${{ matrix.os }}
+
+    steps:
+      - name: Get code
+        uses: actions/checkout@v2
+
+      - name: Use OCaml ${{ matrix.ocaml-version }}
+        uses: avsm/setup-ocaml@v1
+        with:
+          ocaml-version: ${{ matrix.ocaml-version }}
+
+      - name: Build
+        run: |
+          eval $(opam env)
+          opam pin add . -y
+
+      - name: Format
+        run: |
+          eval $(opam env)
+          opam install fromager
+          fromager
+```
---
+++ src/main.ml
@@ -0,0 +1,108 @@
+open Core
+
+(* configuration *)
+
+let fromager_config = "fromage.toml"
+
+type config = {
+  ignored_files: string list;
+  ignored_dirs: string list;
+}
+
+let parse_toml filename : config option =
+  match Sys.file_exists filename with
+  | `No | `Unknown -> None
+  | `Yes -> (
+    let toml = match Toml.Parser.from_filename filename with
+    | `Error _ -> failwith "could not parse fromage.toml"
+    | `Ok toml -> toml 
+    in
+    let config = match Toml.Types.Table.find (Toml.Min.key "config") toml with
+      | TTable config -> config
+      | _ -> failwith "fromage.toml has no config table"
+    in
+    let ignored_files = match Toml.Types.Table.find (Toml.Min.key "ignored_files") config with 
+    | TArray (NodeString ignored_files) -> ignored_files
+    | _ -> []
+    in
+    let ignored_dirs = match Toml.Types.Table.find (Toml.Min.key "ignored_dirs") config with 
+    | TArray (NodeString ignored_dirs) -> ignored_dirs
+    | _ -> []
+    in 
+    Some {
+      ignored_files;
+      ignored_dirs;
+    }
+    )
+
+
+(* interaction with ocamlformat *)
+
+let ocamlformat args =
+  let args = String.concat ~sep:" " args in
+  let command = "ocamlformat " ^ args in
+  Sys.command_exn command
+
+let run_ocamlformat ~(write :bool) (filename:string) = 
+  let args = if write then ["-i"] else [""] in (* write in-place *)
+  let args = args @ [filename] in
+  let _ = ocamlformat args in
+  ()
+
+(* recursion *)
+
+let is_ignored_dir path ~config =
+  match config with 
+  | None -> false
+  | Some config -> List.mem ~equal:String.( = ) config.ignored_dirs path
+
+let is_ignored_file path ~config =
+  match config with 
+  | None -> false
+  | Some config -> List.mem ~equal:String.( = ) config.ignored_files path
+  
+
+(* apply [~f] on every `.ml` and `.mli` file found in [folders] *)
+let rec visit folders ~config ~f =
+  match folders with
+  | [] -> ()
+  | file :: rest -> (
+      let is_dir = Sys.is_directory file in
+      match is_dir with
+      | `Yes ->
+          let rest = 
+            let to_ignore = is_ignored_dir ~config file in
+            if not to_ignore then (
+              let inside_dirs = Sys.ls_dir file in
+              let inside_dirs = List.map inside_dirs ~f:(fun x -> Filename.concat file x) in
+              inside_dirs @ rest) 
+            else rest 
+            in
+          visit rest ~config ~f
+      | `No | `Unknown ->
+        let to_ignore = is_ignored_file ~config file in
+        if not to_ignore then 
+          (match Filename.split_extension file with
+          | (_, Some ".ml") | (_, Some ".mli") -> f file
+          | _ -> ()
+          );
+          visit rest ~config ~f)
+
+
+(* main *)
+
+let () =
+  let cwd = Sys.getcwd () in
+
+  (* parse fromage.toml *)
+  let config = parse_toml fromager_config in
+
+  (* create an empty .ocamlformat *)
+  let ocamlformat_file = Filename.concat cwd ".ocamlformat" in
+  Out_channel.write_all ocamlformat_file ~data:"";
+
+  (* format everything *)
+  visit ["."] ~config ~f:(run_ocamlformat ~write:true);
+
+  (* remove the .ocamlformat *)
+  Unix.remove ocamlformat_file
